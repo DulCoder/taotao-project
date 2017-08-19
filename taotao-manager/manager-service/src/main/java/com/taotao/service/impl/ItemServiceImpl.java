eea619407cf3699cca02b38f945ddbcb4f684a84
specime <zhengxianyou1994@163.com>
2017-08-19 22:25:33 +0800

    首次提交,已整合ssm框架

---
+++ taotao-manager/manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,39 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.service.ItemService;
+
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	@Autowired
+	private TbItemMapper itemMapper;
+
+	@Override
+	public TbItem geItemById(long itemId) {
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+				//添加查询条件
+//				TbItemExample example = new TbItemExample();
+//				TbItemExample.Criteria criteria = example.createCriteria();
+//				criteria.andIdEqualTo(itemId);
+//				List<TbItem> list = itemMapper.selectByExample(example);
+//				if (list != null && list.size() > 0) {
+//					TbItem item = list.get(0);
+//					return item;
+//				}
+		if (item != null){
+
+			return item;
+		}
+		return null;
+	}
+
+}
---
+++ taotao-manager/manager-web/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<span style="color: cornflowerblue;font-size: large">Hello World!!!!</span>
+</body>
+</html>
